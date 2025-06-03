837c476235315bd5d3730669095f31db4d4f1ec4
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-06-04 01:22:20 +0300

    Create test data

---
+++ prepare_test_data.py
@@ -0,0 +1,41 @@
+import os
+from os.path import join, isfile, splitext
+from tqdm.auto import tqdm
+from udapi.block.read.conllu import Conllu
+categories = {
+	'ADJ': ['Gender', 'Number', 'Case'],
+	'NOUN': ['Number', 'Case'],
+	'NOUN': ['Number', 'Case'],
+	'DET': ['Case', 'Gender'],
+	'PRON': ['Case'],
+	'NUM': ['Case'],
+}
+olddir = os.getcwd()
+source = '../UD_Russian-SynTagRus'
+os.chdir(source)
+files = [name for name in os.listdir('.')
+		 if isfile(name) and splitext(name)[1] == '.conllu']
+conllu = Conllu(files=files)
+documents = conllu.read_documents()
+data = set[tuple[str, str]]()
+for document in documents:
+	for tree in tqdm(document.trees):
+		for node in tree.descendants:
+			form = node.form
+			lemma = node.lemma
+			upos = node.upos
+			if upos in categories:
+				features = ['+' + node.feats[category]
+				for category in categories[upos]]
+				if (all(feature != '+' for feature in features) and form.isalpha()
+				and len(form) > 1 and form.islower()):
+					tag = ''.join(features)
+					data.add((form, lemma + tag))
+result = sorted(data, key=lambda x: x[1])
+os.chdir(olddir)
+with open('in.txt', 'w', encoding='utf-8') as fout:
+	for form, analysis in result:
+		fout.write(analysis + '\n')
+with open('corr.txt', 'w', encoding='utf-8') as fout:
+	for form, analysis in result:
+		fout.write(form + '\n')
