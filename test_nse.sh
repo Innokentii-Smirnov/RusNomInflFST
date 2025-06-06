5ed13260cae1883eb1236cc68e322147a923d19f
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-06-06 19:47:37 +0300

    Minor corrections 2

--- test_nse.sh
+++ test_nse.sh
@@ -1,4 +1,4 @@
 ./run_submodule_alone.sh ParadigmFeatures/NonStandEndings/GetNonStandEndings.foma
 clear
 grep -F +Soglbch ParadigmFeatures/NonStandEndings/out.txt
-grep -w ^четыре ParadigmFeatures/NonStandEndings/out.txt
+grep -w $1 ParadigmFeatures/NonStandEndings/out.txt
