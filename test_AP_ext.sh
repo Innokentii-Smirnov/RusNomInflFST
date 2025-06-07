2aca4e13da692afc3d491f14857369a882224abd
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-06-07 09:32:05 +0300

    Lexical work

--- test_AP_ext.sh
+++ test_AP_ext.sh
@@ -1,6 +1,8 @@
+clear
 ./run_submodule_alone.sh ParadigmFeatures/Alternations/GetAlternations.foma
 ./run_submodule_alone.sh ParadigmFeatures/NonStandEndings/GetNonStandEndings.foma
 ./run_submodule_alone.sh ConvRepr/Stem/GetStem
 ./run_submodule_alone.sh ParadigmFeatures/Star/GetStar.foma
 ./run_submodule_alone.sh ParadigmFeatures/AP/GetAP.foma
-grep -E -w '^(клок|кол|копыл|крюк|лист|лоскут|прут|сук)' ParadigmFeatures/AP/out.txt
+# grep -E -w '^(клок|кол|копыл|крюк|лист|лоскут|прут|сук)' ParadigmFeatures/AP/out.txt
+grep -E -w '^(щенок|муж|сын|шурин)' ParadigmFeatures/AP/out.txt
