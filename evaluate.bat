f501ca681a1e09ebff89c9f49836292efcde20c4
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-27 01:05:06 +0300

    Simplify evaluation script call

--- evaluate.bat
+++ evaluate.bat
@@ -1,5 +1,2 @@
 @echo off
-set olddir=%cd%
-cd TestMorph
-python evaluate.py "../src/Morphonology/ConvToOrth/out.txt" "../test_data/corr.txt" "../test_result"
-cd %olddir%
+python TestMorph\evaluate.py "src\Morphonology\ConvToOrth\out.txt" "test_data\corr.txt" "test_result"
