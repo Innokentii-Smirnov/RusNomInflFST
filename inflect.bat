33a5fd3c4049ecae971c9152524ca1d6864d4078
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-27 02:09:13 +0300

    Adjust get_paradigm_scheme call to the submodule name

--- inflect.bat
+++ inflect.bat
@@ -1,3 +1,3 @@
 @echo off
 chcp 65001
-echo %1 | lookup src\ParadigmFeatures\GrammRazr\GetGrammRazr.bin -q -flags xLL -utf8 | python python\get_paradigm_scheme.py grammSystem\RusNomInfl | lookup src\Morphonology\ConvToOrth\ConvToOrth.bin -q -utf8 | findstr /v "^$"
+echo %1 | lookup src\ParadigmFeatures\GrammRazr\GetGrammRazr.bin -q -flags xLL -utf8 | python ParadigmScheme\get_paradigm_scheme.py grammSystem\RusNomInfl | lookup src\Morphonology\ConvToOrth\ConvToOrth.bin -q -utf8 | findstr /v "^$"
