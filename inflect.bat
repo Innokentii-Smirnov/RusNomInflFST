bf006a6d3cc10ccb5def91f293c077ae1f1331b5
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-27 01:36:49 +0300

    Add .bat file for getting a single word paradigm

---
+++ inflect.bat
@@ -0,0 +1,2 @@
+chcp 65001
+echo %1 | lookup src\ParadigmFeatures\GrammRazr\GetGrammRazr.bin -q -flags xLL -utf8 | python python\scripts\get_paradigm_scheme.py | lookup src\Morphonology\ConvToOrth\ConvToOrth.bin -q -utf8
