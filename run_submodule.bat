fa4f80f184b8a495fc43bef188c1e8a890af13d8
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-26 18:38:07 +0300

    Adjust compilation script for relative paths

--- run_submodule.bat
+++ run_submodule.bat
@@ -1,21 +1,20 @@
 @echo off
-set filepath="%1"
-for /F "delims=" %%i in (%filepath%) do set directory="%%~dpi"
-for /F "delims=" %%i in (%filepath%) do set name="%%~ni"
+set filepath=%1
+for /F "delims=" %%i in ("%filepath%") do set directory=%%~dpi
+for /F "delims=" %%i in ("%filepath%") do set name=%%~ni
 set binary=%name%.bin
 set olddir=%cd%
 cd %directory%
 xfst -utf8 -e "source %name%.foma" -e "push %name%" -e "invert net" -e "save stack %binary%" -e "sigma" -stop
 echo "Created Foma binary %binary%."
 if exist in.txt (
-	set input=in.txt
-	lookup %binary% -utf8 < %input% > loc.txt
+	lookup %binary% -utf8 < in.txt > loc.txt
 )
-set beginning=%directory:~33,16%
+set beginning=%filepath:~4,16%
 if %beginning% == ParadigmFeatures (
 	set input=../../../test_data/lemmata.txt
 ) else (
 	set input=../../../test_data/in.txt
 )
 lookup %binary% -utf8 < %input% > out.txt
-cd "%olddir%"
+cd %olddir%
