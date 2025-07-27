7a446bb01f96583bdb6509933f1143623a11b5c0
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-27 14:18:44 +0300

    Correct paradigm defects, add cls after compilation

--- scheme.bat
+++ scheme.bat
@@ -1,3 +1,4 @@
 @echo off
+cls
 chcp 65001
 echo %1 | lookup src\ParadigmFeatures\GrammRazr\GetGrammRazr.bin -q -flags xLL -utf8 | python ParadigmScheme\get_paradigm_scheme.py grammSystem\RusNomInfl | lookup src\Inflection\ParadigmDefects\ParadigmDefects.bin -q -utf8 | findstr /v "^$"
