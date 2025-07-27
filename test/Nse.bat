2eeed0fbffa78f97acf010a005b3c330b836de47
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-27 16:21:19 +0300

    Correct Nse test

--- test/Nse.bat
+++ test/Nse.bat
@@ -1,8 +1,8 @@
 @echo off
 chcp 65001
 if %1 == hide (
-  run_submodule.bat src\ParadigmFeatures\Alternations\GetAlternations.foma
+  run_submodule.bat src\ParadigmFeatures\NonStandEndings\GetNonStandEndings.foma
   cls
 ) else (
-  run_submodule.bat src\ParadigmFeatures\Alternations\GetAlternations.foma
+  run_submodule.bat src\ParadigmFeatures\NonStandEndings\GetNonStandEndings.foma
 )
